45c58bc1af23796e3933980008add14a7fb0c1c5
1711824 <1711824@DASOE6714D>
2018-10-04 20:30:40 -0300

    aula do dia 04 10

---
+++ WebApplication1/Pages/Usuarios/Cadastrar.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Data;
+
+namespace WebApplication1.Pages.Usuarios
+{
+    public class CadastrarModel : PageModel
+    {
+        [BindProperty]
+
+        public Usuario usuario { get; set; }
+        public void OnGet(string nome, string senha)
+        {
+            if (usuario == null)
+            {
+                usuario = new Usuario();
+            }
+            usuario.Nome = nome;
+            usuario.senha = senha;
+            
+        }
+    }
+}
